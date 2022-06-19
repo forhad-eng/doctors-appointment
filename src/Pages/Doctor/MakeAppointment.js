3a84e4fa86813042a11ed8e3065583e9d0a8ef61
forhad-eng <forhadu.cse@gmail.com>
2022-06-19 16:57:16 +0600

    treatment section done and task completed

--- src/Pages/Doctor/MakeAppointment.js
+++ src/Pages/Doctor/MakeAppointment.js
@@ -1,4 +1,4 @@
-import { faBell } from '@fortawesome/free-solid-svg-icons'
+import { faBell, faPhone, faVideo } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import Examinations from './Examinations'
 
@@ -14,7 +14,7 @@ const MakeAppointment = () => {
                 </span>
             </div>
 
-            <div className="my-8 bg-[#F1E9FE] rounded-2xl p-4">
+            <div className="mt-8 mb-6 bg-[#F1E9FE] rounded-2xl p-4">
                 <h3 className="text-xl font-bold">Consults For Today</h3>
                 <div className="my-4">
                     {[...Array(31).keys()].map(key => (
@@ -29,7 +29,7 @@ const MakeAppointment = () => {
                 </div>
             </div>
 
-            <div className="my-8 bg-[#F1E9FE] rounded-2xl p-4">
+            <div className="my-6 bg-[#F1E9FE] rounded-2xl p-4">
                 <div className="flex justify-between">
                     <h3 className="text-xl font-bold">Examinations</h3>
                     <h4 className="text-primary font-bold">See All</h4>
@@ -39,6 +39,40 @@ const MakeAppointment = () => {
                     <Examinations date="23 Dec 2020" specialized="Heart Surgeon" doctor="Ellen Bradly" />
                     <Examinations date="24 Nov 2020" specialized="Medicine Specialist" doctor="Luke McKinney" />
                     <Examinations date="15 May 2020" specialized="Backpain Specialist" doctor="Cameroon Holland" />
+                </div>
+            </div>
+
+            <div className="my-6 bg-[#F1E9FE] rounded-2xl p-4">
+                <h3 className="text-xl font-bold">Treatment</h3>
+                <div className="treatment mt-3">
+                    <div className="border-r-2 border-gray-300 pr-10">
+                        <p className="text-gray-500">November 2020</p>
+                        <div className="text-primary font-bold bg-white rounded-2xl flex justify-center gap-5 mt-6 py-6 px-10">
+                            {' '}
+                            &lt; <span className="line-through">26</span> <span>27</span> <span>28</span>{' '}
+                            <span className="line-through">29</span> <span className="line-through">30</span>{' '}
+                            <span className="line-through">31</span> <span className="line-through">01</span> &gt;
+                        </div>
+                    </div>
+                    <div className="flex gap-3 bg-[#F5F7F9] rounded-2xl p-4">
+                        <div className="text-white bg-secondary rounded-xl flex flex-col justify-center items-center py-7">
+                            <p className="text-2xl font-bold">27</p>
+                            <p className="px-2">November</p>
+                        </div>
+                        <div>
+                            <p className="font-bold">Medicine</p>
+                            <p className="text-gray-500"><small>11:30 AM</small></p>
+
+                            <div className="flex gap-2 ml-8 mt-4">
+                                <button className="w-10 h-10 bg-white rounded-full">
+                                    <FontAwesomeIcon icon={faPhone} className="text-secondary" />
+                                </button>
+                                <button className="w-10 h-10 bg-white rounded-full">
+                                    <FontAwesomeIcon icon={faVideo} className="text-primary" />
+                                </button>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
